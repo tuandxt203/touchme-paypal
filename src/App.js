ba1427f3b5fb284522dec755c76ae3820d845446
Đặng Xuân Tuấn <116486692+tuandxt203@users.noreply.github.com>
2023-11-14 10:23:27 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 function App() {
+  function _createOrder(data, actions) {
+    // Kiểm tra nếu có giá trị value được truyền từ URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const valueParam = urlParams.get("value");
+    const value = valueParam ? valueParam : "1"; // Mặc định là 1 nếu không có giá trị truyền vào từ URL
+
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: value,
+          },
+        },
+      ],
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PayPalButton
+        createOrder={(data, actions) => _createOrder(data, actions)}
+      />
     </div>
   );
 }
-
 export default App;
